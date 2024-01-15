ce2e79d3537e6f559910a5d8ef47b95136271e6e
Willmp4 <54776440+Willmp4@users.noreply.github.com>
2024-01-15 10:37:06 +0000

    Fix Document class initialization and add to_dict method

--- models/Document.py
+++ models/Document.py
@@ -1,11 +1,20 @@
 
 class Document:
-    def init(self, document_id, title, content, biography, upload_date):
+    def __init__(self, document_id, title, content, biography, upload_date):
         self.document_id = document_id
         self.title = title
         self.content = content
         self.biography = biography
         self.upload_date = upload_date
+
+    def to_dict(self):
+        return {
+            'document_id': self.document_id,
+            'title': self.title,
+            'content': self.content,
+            'biography': self.biography,
+            'upload_date': self.upload_date
+        }
 
     def upload(self, db):
         db['documents'].append(self)
